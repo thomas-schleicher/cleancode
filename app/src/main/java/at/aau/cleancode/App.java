29617f3900ebb1b66e728413b81d9327cec22f23
Florian <flo.hager12@gmail.com>
2025-03-03 16:17:36 +0100

    yeet

--- app/src/main/java/at/aau/cleancode/App.java
+++ app/src/main/java/at/aau/cleancode/App.java
@@ -1,13 +1,52 @@
 package at.aau.cleancode;
 
-public class App {
+import at.aau.cleancode.utility.Validator;
+import java.util.Scanner;
 
+public class App {
+    private static final Scanner SCANNER = new Scanner(System.in);
 
     public static void main(String[] args) {
-        App application = new App();
+        printInformation();
     }
 
-    public App() {
-        
+    private static void printInformation() {
+        System.out.println("This is a simple web crawler, that crawls a given URL to a given depth.");
+        while (true) {
+            System.out.println("Following actions are possible:");
+            System.out.println("1. Enter a URL to be crawled");
+            System.out.println("2. Exit the program");
+            System.out.println("Please Enter the action you wish to perform: ");
+            String action = SCANNER.nextLine();
+            if (Validator.validateAction(action)) {
+                performAction(Integer.parseInt(action));
+            }
+        }
+    }
+
+    private static void performAction(int action) {
+        switch (action) {
+            case 1:
+                System.out.println("Enter a URL to be crawled: ");
+                String url = SCANNER.nextLine();
+                if (!Validator.checkURL(url)) {
+                    System.out.println("Invalid URL");
+                    return;
+                }
+                System.out.println("Enter the Depth the crawler should crawl (optional): ");
+                String depthInput = SCANNER.nextLine();
+                int depth = Validator.checkInputDepth(depthInput) ? Integer.parseInt(depthInput) : 0;
+                System.out.println("Crawling URL: " + url + " to a depth of " + depth);
+                //todo: @Thomas: crawl here mit url und depth
+                break;
+            case 2:
+                System.out.println("Exiting the program");
+                SCANNER.close();
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Invalid Action");
+                break;
+        }
     }
 }
---
+++ app/src/main/java/at/aau/cleancode/utility/Validator.java
@@ -0,0 +1,44 @@
+package at.aau.cleancode.utility;
+
+public class Validator {
+
+
+    public static boolean checkInputDepth(String depthInput) {
+        if (depthInput.isBlank()) {
+            return false;
+        }
+        try {
+            int depth = Integer.parseInt(depthInput);
+            if (depth < 0) {
+                System.out.println("Depth cannot be negative");
+                return false;
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid Depth format");
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean checkURL(String url) {
+        // Regex explanation:
+        // ^https?://      : Starts with "http://" or "https://"
+        // www\\.         : Must include "www." right after the protocol (escaped dot)
+        String regex = "^(https?)://www\\..+$";
+        return url != null && url.matches(regex);
+    }
+
+    public static boolean validateAction(String action) {
+        if (action == null || action.isEmpty()) {
+            System.out.println("Action cannot be null or empty");
+            return false;
+        }
+        try {
+            Integer.parseInt(action);
+        } catch (NumberFormatException e) {
+            System.out.println("The selected action must be a number");
+            return false;
+        }
+        return true;
+    }
+}
